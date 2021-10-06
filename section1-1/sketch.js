d750ea901f1d54ecafc6b623dffbcf834206f9ab
OkazakiHaruka24 <92011722+OkazakiHaruka24@users.noreply.github.com>
2021-10-06 15:32:50 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -4,13 +4,13 @@ function setup() {
 
 function draw() {
   background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
+  strokeWeight(10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
+  fill(178, 100, 162);
+  triangle(12,120,116,120,116,10);
   fill(255);
-  textSize(32);
+  textSize(15);
   textFont("serif");
   text("46", 68, 100);
 }
