0e59cc4fb879d78821105683212117b17e68b6fa
OkazakiHaruka24 <92011722+OkazakiHaruka24@users.noreply.github.com>
2021-11-02 01:53:05 +0900

    Changes section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -12,5 +12,10 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  if(keyIsDown(" ".charCodeAt(0))){count=(count+2)%cycle;}
+  else{count=(count+1)% cycle;}
+  if(count>60){size=count}
+  else{size=100-count}
+
   ellipse(width / 2, height / 2, size);
 }
